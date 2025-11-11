b477de7e87376b67877a004cdc693e8257eb9b45
AiKunLa <3535648608@qq.com>
2025-11-11 21:12:20 +0800

    更新依赖项和配置：添加了 @types/node、path 和 sass；配置了 TypeScript 路径和 Vite 别名以支持 SCSS。

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 注意：结尾必须有分号！
+        additionalData: `@import "@/styles/variables.scss";`
+      }
+    }
+  }
 })
